4739184014a2531c6509aef25fd07b11a9fe29ee
Julia Shen <juliashen@Julias-MacBook-Pro-230.local>
2025-04-09 05:24:06 -0500

    added tables chairs and stove, working on cutting board

---
+++ scripts/chair_left.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var player = get_tree().get_first_node_in_group("player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+
+func _on_interact():
+#	$CollisionShape2D.disabled = true
+	player.position = $TextureRect.global_position
+	player.position.x += 18
+	player.position.y -= 8
+	player.update_sit_animation(false)
+	
---
+++ scripts/stove.gd
@@ -0,0 +1,39 @@
+extends StaticBody2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var player = get_tree().get_first_node_in_group("player")
+
+var SPEED: float = 1
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$ProgressBar.visible = false
+	$Timer.wait_time /= SPEED
+	$Pan.visible = false
+	interaction_area.interact = Callable(self, "_on_interact")
+
+func _on_interact():
+#	$CollisionShape2D.disabled = true
+	$ProgressBar.visible = true
+	$Timer.start()
+	$Pan.visible = true
+	player.position = $TextureRect.global_position
+	player.position.y += 8
+	player.position.x += 20
+	player.update_animation("idleBack")
+	
+func _process(delta):
+	if $ProgressBar.visible and player.position.distance_to(position) < 50:
+		$ProgressBar.value = ($Timer.wait_time - $Timer.time_left)/$Timer.wait_time*100
+	else:
+		$Timer.stop()
+		$ProgressBar.visible = false
+		$Pan.visible = false
+	
+
+func _on_timer_timeout():
+	$ProgressBar.visible = false
+	$Pan.visible = false
+
